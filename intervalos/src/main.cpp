6f0a313fe70fb7e933a220f0b84300a3b4b79419
SergioVictorSS <37191920+SergioVictorSS@users.noreply.github.com>
2022-04-07 22:10:33 -0300

    Implementação da questão

--- intervalos/src/main.cpp
+++ intervalos/src/main.cpp
@@ -13,9 +13,29 @@ using std::setprecision;
 
 // Se desejar, crie funções aqui, antes do main().
 
-int main(void)
+int main(int argc, char * argv[])
 {
-    // TODO: Adicione aqui a sua solução.
-
+    int x;
+    int qtd = 0;
+    int qtdIntervalos [] = {0,0,0,0};
+    int out = 0;
+    while( cin >> std::ws >> x) {
+        if(x >= 0 && x <= 25 ){
+          qtdIntervalos[0]++;
+        } else if (x >= 0 && x < 25 ) {
+          qtdIntervalos[1]++;
+        }else if (x >= 25 && x < 75 ) {
+          qtdIntervalos[2]++;
+        }else if (x >= 75 && x < 100 ) {
+          qtdIntervalos[3]++;
+        }else{
+          out++;
+        }
+        qtd++;
+    }
+    for(int qtds : qtdIntervalos){
+      std::cout << std::setprecision(4) << qtds/qtd*100 << "\n";
+    }
+        std::cout << std::setprecision(4) << out/qtd*100 << "\n";
     return 0;
 }
