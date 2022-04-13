994c9e9013af9f177e53d9fe8a650361b15305bd
SergioVictorSS <37191920+SergioVictorSS@users.noreply.github.com>
2022-04-12 21:13:43 -0300

    Implementação da função

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -6,7 +6,15 @@ const int SIZE = 5; // input size.
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
+  int saida = 0;
+  for(int i = 0; i<SIZE;i++){
+    int x = 0;
+    std::cin >> x;
+    if(x<0){
+      saida++;
+    }
+  }
+  cout << saida;
 
     return 0;
 }
