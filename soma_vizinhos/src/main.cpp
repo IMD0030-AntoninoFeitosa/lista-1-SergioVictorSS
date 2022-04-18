45e609631aba72ca04cea1cb6557e114a85e60e6
SergioVictorSS <37191920+SergioVictorSS@users.noreply.github.com>
2022-04-17 21:32:27 -0300

    Implementação da função

--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -4,13 +4,18 @@
  * @data June, 6th 2021
  */
 #include <iostream>
+#include <cmath>  
 using std::cout;
 using std::cin;
 using std::endl;
 
 int main( void )
 {
-    // TODO: Adicione seu código aqui. 
+    int x, y = 0;
+    while( cin >> std::ws >> x >> y) {   
+        y = x + y - (y == 0 ? 0 : y/abs(y));
+        cout << (x + y) * ((x > y ? x - y : y - x) + 1) / 2<< '\n';
+    }
 
     return 0;
 }
